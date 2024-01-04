aadf0e0a4c1cffb0549b4a3148c647776c273892
ductuyen1108 <sasuke08chelsea@gmail.com>
2024-01-05 00:35:26 +0700

    up

--- src/statistical/count-student/common/hooks/useGetClassAndCount.ts
+++ src/statistical/count-student/common/hooks/useGetClassAndCount.ts
@@ -5,7 +5,7 @@ import { IParamsClassAndInfo } from '../interface';
 
 export const useGetClassAndCount = (params: IParamsClassAndInfo) => {
   const { data: classAndCountData, isLoading: isLoadingClassAndCount } = useQuery(
-    [QUERY_KEYS.GET_CLASS_AND_COUNT],
+    [QUERY_KEYS.GET_CLASS_AND_COUNT, params],
     () => getClassAndCount(params)
   );
   return { classAndCountData, isLoadingClassAndCount }
